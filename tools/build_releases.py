a763788d415fc8f5d6e977d185604a06c0b33e8c
willems davy <joyrider3774@hotmail.com>
2026-09-19 13:23:13 +0200

    remove forced screenbuffer 0 for gamebuino meta

--- tools/build_releases.py
+++ tools/build_releases.py
@@ -90,7 +90,7 @@ SKINS = 4
 # (device, variant added to the file name, defines)
 TARGETS = [
     ("ESPboy", "", {}),
-    ("GamebuinoMeta", "", {"SCREENBUFFER": 0}),
+    ("GamebuinoMeta", "", {}),
     ("PyBadge", "", {}),
     ("PyGamer", "", {}),
     ("PicoSystem", "", {}),
